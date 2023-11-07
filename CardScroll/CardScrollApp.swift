364c5147e106af4b2603d2f32ffbaf99a5bab6bb
Jawad Ali <jawad@eddress.co>
2023-11-07 22:40:30 +0100

    Initial Commit

---
+++ CardScroll/CardScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardScrollApp.swift
+//  CardScroll
+//
+//  Created by jawad ali on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
